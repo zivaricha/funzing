ee2a84e9a36c03e1421bf1091e282fc5b63d7b4d
Ziv Aricha <zivaricha@gmail.com>
2017-12-18 13:10:15 +0000

    added scopes to Activity model

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,6 @@
 class ActivitiesController < ApplicationController
   def index
-    @activities = Activity.all.order(:date)
+    @activities = Activity.has_tickets_left.order_by_date
   end
   
   def show
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -5,4 +5,9 @@ class Activity < ActiveRecord::Base
   
   # validations
   validates_presence_of :title, :description, :user
+  
+  # scopes
+    scope :has_tickets_left, -> { where("participants < max_participants") }
+    scope :order_by_date, -> { order(:date)}
+  
 end
